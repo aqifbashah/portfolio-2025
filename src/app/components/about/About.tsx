591eb6d32b6e3256113bfd6d935933aa440a657d
aqifbashah <aqifbashah@gmail.com>
2025-02-19 00:11:25 +0800

    wip: add basic component to about page.

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -1,7 +1,11 @@
 export default function About() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-4">
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-10 py-20">
       <h1 className="text-5xl">About</h1>
+      <div className="flex h-full w-5/6 gap-20 px-20">
+        <div className="h-full w-full rounded-xl bg-primary"></div>
+        <div className="h-full w-full rounded-xl bg-secondary"></div>
+      </div>
     </div>
   );
 }
